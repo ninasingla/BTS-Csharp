5851b696577c3c421cb19f1d1dea7e3bb1bb43f1
Nina Singla <ninasingla@me.com>
2025-10-17 18:47:21 +0200

    01-NombreCache : update - exercice terminé

--- 01-NombreCache/NombreCache/Program.cs
+++ 01-NombreCache/NombreCache/Program.cs
@@ -1,15 +1,86 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NombreCache
 {
-    internal class Program
+    /// <summary>
+    /// Jeu du nombre caché
+    /// </summary>
+    class Program
     {
         static void Main(string[] args)
         {
+            // déclaration
+            int valeur = 0, essai = 0, nombre = 1;
+            bool correct;
+
+            // saisie du nombre à chercher
+            correct = false;
+            while (!correct)
+            {
+                try
+                {
+                    Console.Write("Entrez le nombre à chercher : ");
+                    valeur = int.Parse(Console.ReadLine());
+                    correct = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Erreur de saisie : saisissez un nombre entier");
+                }
+            }
+            Console.Clear();
+
+            // saisie du premier essai
+            correct = false;
+            while (!correct)
+            {
+                try
+                {
+                    Console.Write("Entrez un essai : ");
+                    essai = int.Parse(Console.ReadLine());
+                    correct = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Erreur de saisie : saisissez un nombre entier");
+                }
+            }
+
+            // boucle sur les essais
+            while (essai != valeur)
+            {
+                // test de l'essai par rapport à la valeu à chercher
+                if (essai > valeur)
+                {
+                    Console.WriteLine(" --> trop grand !");
+                }
+                else
+                {
+                    Console.WriteLine(" --> trop petit !");
+                }
+                // saisie d'un nouvel essai
+                correct = false;
+                while (!correct)
+                {
+                    try
+                    {
+                        Console.Write("Entrez un essai : ");
+                        essai = int.Parse(Console.ReadLine());
+                        correct = true;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Erreur de saisie : saisissez un nombre entier");
+                    }
+                }
+                // compteur d'essai
+                nombre++;
+            }
+
+            // valeur trouvée
+            Console.WriteLine("Bravo ! Vous avez trouvé en " + nombre + " fois !");
+
+            Console.ReadLine();
         }
     }
 }
